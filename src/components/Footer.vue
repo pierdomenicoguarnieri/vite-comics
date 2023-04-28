4912398269dd7426c3145a069fade305ee648c00
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-04-28 14:23:11 +0200

    add: FooterBottom partial

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,16 +1,12 @@
 <script>
 import FooterTop from "./partials/FooterTop.vue";
+import FooterBottom from "./partials/FooterBottom.vue";
 import {menuFooter, menuSocial} from "../data/menus.js";
 export default {
   name: "Footer",
   components:{
-    FooterTop
-  },
-  data(){
-    return{
-      menuFooter,
-      menuSocial
-    }
+    FooterTop,
+    FooterBottom
   }
 }
 </script>
@@ -18,71 +14,10 @@ export default {
 <template>
   <footer>
     <FooterTop/>
-    <div class="bottom">
-      <div class="container">
-        <div class="left">
-          <span>Sing-up now!</span>
-        </div>
-        <div class="right">
-          <span>Follow us</span>
-          <nav>
-            <ul>
-              <li v-for="(icon, index) in menuSocial" :key="index">
-                <a :href="icon.href"><img :src="icon.img" alt=""></a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
+    <FooterBottom/>
   </footer>
 </template>
 
-<style lang="scss" scoped>
-  @use "../scss/partial/mixin" as *;
-  @use "../scss/partial/variables" as *;
+<style>
 
-  .container{
-    @include container;
-    @include displayFlex("between");
-    height: 100%;
-  }
-  .bottom{
-    background-color: $grey;
-    padding: 30px 0;
-    @include displayFlex("both");
-    .left{
-      @include displayFlex("vertical");
-      span{
-        padding: 10px 10px;
-        border: 2px solid $blue;
-        color: $white;
-        text-transform: uppercase;
-      }
-    }
-    .right{
-      @include displayFlex("vertical");
-      span{
-        color: $blue;
-        font-size: $xl-text;
-        text-transform: uppercase;
-        font-weight: $bold;
-      }
-      nav{
-        @include displayFlex("vertical");
-        height: 100%;
-        ul{
-          margin-left: 25px;
-        @include displayFlex("none");
-          li:not(:last-child){
-            margin-right: 15px;
-          }
-          a:hover{
-            transition: all .3s;
-            filter: brightness(60%);
-          }
-        }
-      }
-    }
-  }
 </style>
---
+++ src/components/partials/FooterBottom.vue
@@ -0,0 +1,80 @@
+<script>
+import {menuSocial} from "../../data/menus.js";
+export default {
+  name: "FooterBottom",
+  data(){
+    return{
+      menuSocial
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="bottom">
+    <div class="container">
+      <div class="left">
+        <span>Sing-up now!</span>
+      </div>
+      <div class="right">
+        <span>Follow us</span>
+        <nav>
+          <ul>
+            <li v-for="(icon, index) in menuSocial" :key="index">
+              <a :href="icon.href"><img :src="icon.img" alt=""></a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @use "../../scss/partial/mixin" as *;
+  @use "../../scss/partial/variables" as *;
+
+  .container{
+    @include container;
+    @include displayFlex("between");
+    height: 100%;
+  }
+  .bottom{
+    background-color: $grey;
+    padding: 30px 0;
+    @include displayFlex("both");
+    .left{
+      @include displayFlex("vertical");
+      span{
+        padding: 10px 10px;
+        border: 2px solid $blue;
+        color: $white;
+        text-transform: uppercase;
+      }
+    }
+    .right{
+      @include displayFlex("vertical");
+      span{
+        color: $blue;
+        font-size: $xl-text;
+        text-transform: uppercase;
+        font-weight: $bold;
+      }
+      nav{
+        @include displayFlex("vertical");
+        height: 100%;
+        ul{
+          margin-left: 25px;
+        @include displayFlex("none");
+          li:not(:last-child){
+            margin-right: 15px;
+          }
+          a:hover{
+            transition: all .3s;
+            filter: brightness(60%);
+          }
+        }
+      }
+    }
+  }
+</style>
